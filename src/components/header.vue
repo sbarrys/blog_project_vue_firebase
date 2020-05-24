ed55cf1b3393b01e64f9c2dab98ba7c6563091fe
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-05-25 03:31:33 +0900

    drawer 간단 설정

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,7 +4,7 @@
       color="primary lighten-1"
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer=true"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Page title</v-toolbar-title>
 
@@ -39,5 +39,44 @@
         </v-list>
       </v-menu>
     </v-app-bar>
+
+        <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
   </div>
 </template>
+<script>
+export default {
+  data: () => ({
+    drawer: false
+  })
+}
+</script>

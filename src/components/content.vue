79e01d510583803c40d2282248eb6cac37d075cd
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-05-25 03:25:34 +0900

    esLint 설정 / header, content, footer 간단정의

---
+++ src/components/content.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2 v-for="i in 70" :key="i">
+            hello everyone
+        </h2>
+    </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      sixty: 60
+    }
+  }
+}
+</script>
